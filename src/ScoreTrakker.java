bfd2ceda14c7d36c2041d9879eb1c31f603cbf68
Joesterr <101225288+Joesterr@users.noreply.github.com>
2023-09-26 01:52:42 -0600

    comments and references

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,3 +1,20 @@
+/**
+ * ScoreTrakker - A Java program for processing and displaying student scores.
+ * Authors: Jonathan Nunez Toledo, Calvin Tran
+ *
+ * Description:
+ * This program reads student data from text files, processes the information, and
+ * prints the student records in sorted order based on their scores.
+ *
+ * References:
+ * 1. Sorting ArrayList in Java - https://www.javatpoint.com/how-to-sort-arraylist-in-java
+ * 2. Scanner hasNextLine() Method in Java - https://www.geeksforgeeks.org/scanner-hasnextline-method-in-java-with-examples/
+ * 3. Collections in Java - https://www.javatpoint.com/collections-in-java
+ * 4. Java IOException Documentation - https://docs.oracle.com/javase/8/docs/api/java/io/IOException.html
+ * 5. Using the 'finally' Block for Catching Exceptions - https://www.geeksforgeeks.org/java-program-to-use-finally-block-for-catching-exceptions/
+ * 6. Understanding the printStackTrace() Method in Java - https://www.educative.io/answers/what-is-the-printstacktrace-method-in-java
+ */
+
 import java.util.ArrayList;
 import java.io.FileReader;
 import java.util.Collections;
@@ -5,11 +22,11 @@ import java.util.Scanner;
 import java.io.IOException;
 
 class ScoreTrakker {
-    private ArrayList<Student> students = new ArrayList<Student>();
-    private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
+    private ArrayList<Student> students = new ArrayList<Student>(); 
+    private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" }; 
 
     public ScoreTrakker() {
-        students = new ArrayList<>();
+        students = new ArrayList<>(); 
     }
 
     public void loadDataFile(String fileName) throws IOException {
@@ -18,21 +35,24 @@ class ScoreTrakker {
         Scanner scanner = null;
 
         try {
-            reader = new FileReader(fileName);
-            scanner = new Scanner(reader);
+            reader = new FileReader(fileName); // Open the specified file for reading
+            scanner = new Scanner(reader); // Create a Scanner to read from the file
             while (scanner.hasNextLine()) {
-                String name = scanner.nextLine();
-                String scoreError = scanner.nextLine();
+                String name = scanner.nextLine(); // Read student name from the file
+                String scoreError = scanner.nextLine(); // Read potential score error message
                 try {
-                    int score = Integer.parseInt(scanner.nextLine());
-                    students.add(new Student(name, score));
+                    int score = Integer.parseInt(scanner.nextLine()); // Parse and read student score
+                    students.add(new Student(name, score)); // Create a Student object and add it to the ArrayList
                 } catch (NumberFormatException e) {
+                    // Handle the case where the score is not a valid integer
                     System.out.println("Incorrect Format for " + name + " not a valid score: " + scoreError);
                 }
             }
         } catch (IOException e) {
+            // Handle the case where the file cannot be opened
             System.out.println("Can't open file: " + fileName);
         } finally {
+            // Close the file and scanner regardless of whether there was an exception
             if (scanner != null) {
                 scanner.close();
             }
@@ -43,20 +63,22 @@ class ScoreTrakker {
     }
 
     public void printInOrder() {
-        Collections.sort(students);
+        Collections.sort(students); // Sort the students ArrayList based on their scores
         for (Student student : students) {
-            System.out.println(student);
+            System.out.println(student); // Print the students in sorted order
         }
     }
 
     public void processFiles() {
         for (String fileName : files) {
             try {
-                loadDataFile(fileName);
-                printInOrder();
+                loadDataFile(fileName); // Load data from each file
+                printInOrder(); // Print the data in sorted order
             } catch (FileNotFoundException e) {
+                // Handle the case where the file is not found
                 System.out.println("File not found: " + fileName);
             } catch (IOException e) {
+                // Handle the case where there is an error processing the file
                 System.out.println("Error processing file: " + fileName);
             }
         }
@@ -64,8 +86,6 @@ class ScoreTrakker {
 
     public static void main(String[] args) {
         ScoreTrakker scoreTrakker = new ScoreTrakker();
-        scoreTrakker.processFiles();
+        scoreTrakker.processFiles(); // Start processing the specified files
     }
 }
-
-    
--- src/Student.java
+++ src/Student.java
@@ -1,19 +1,39 @@
+/**
+ * Student - Represents a student with a name and a score.
+ */
 class Student implements Comparable<Student> {
-    private String name;
-    private int score;
+    private String name; // Student's name
+    private int score;   // Student's score
 
+    /**
+     * Constructor to initialize a Student object with a name and a score.
+     *
+     * @param name  The name of the student.
+     * @param score The score of the student.
+     */
     public Student(String name, int score) {
         this.name = name;
         this.score = score;
     }
 
+    /**
+     * CompareTo method to allow sorting of Student objects based on their names.
+     *
+     * @param other The other Student object to compare to.
+     */
     @Override
     public int compareTo(Student other) {
         return this.name.compareTo(other.name);
     }
 
+    /**
+     * toString method to provide a string representation of the Student object.
+     *
+     * @return A string containing the student's name and score.
+     */
     @Override
     public String toString() {
         return name + " " + score;
     }
 }
+
