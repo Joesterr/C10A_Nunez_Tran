a3798a4c3b6bee89f67e8a1153c7bea0409f184b
Joesterr <101225288+Joesterr@users.noreply.github.com>
2023-09-26 01:18:19 -0600

    calvin and jonathan typing and brains working i hope

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -6,23 +6,35 @@ import java.util.IOException;
 
 class ScoreTrakker {
     private ArrayList<Student> students = new ArrayList<Student>;
+    private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
 
     public ScoreTrakker() {
         students = new ArrayList<>();
     }
     
-    public void loadDataFile(string fileName) {
-        FileReader reader = new FileReader(fileName);
-        Scanner scanner = new Scanner(reader);
+    public void loadDataFile(string fileName) throws IOException {
+        String line = "";
+        String name = "";
 
-        while (scanner.hasNextLine()) {
-            String name = scanner.nextLine();
-            int score = Integer.parseInt(scanner.nextLine());
-            students.add(new Student(name, score));
+        try {
+            FileReader reader = new FileReader(fileName);
+            Scanner scanner = new Scanner(reader);
+            while (scanner.hasNextLine()) {
+                String name = scanner.nextLine();
+                String scoreError = scanner.nextLine();
+                try {
+                    int score = Integer.parseInt(scanner.nextLine());
+                    students.add(new Student(name, score));
+                } catch (NumberFormatException e) {
+                    System.out.println("Incorrect Format for " + name + " not a valid score: " + scoreError);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Can't open file: " + fileName);
+        } finally {
+            scanner.close();
+            reader.close();
         }
-       
-        scanner.close();
-        reader.close();
     }
 
     public void printInOrder() {
@@ -33,8 +45,16 @@ class ScoreTrakker {
     }
     
     public void processFiles() {
-        loadDataFile("scores.txt");
-        printInOrder();
+        for (String fileName : files) {
+            try {
+                loadDataFile(fileName);
+                printInOrder();
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + fileName);
+            } catch (IOException e) {
+                System.out.println("Error processing file: " + fileName);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -42,3 +62,5 @@ class ScoreTrakker {
         scoreTrakker.processFiles();
     }
 }
+
+    
