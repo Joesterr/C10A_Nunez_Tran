cab66d975381c632f129dfa8c4520dd564bb5da5
Joesterr <101225288+Joesterr@users.noreply.github.com>
2023-09-25 23:50:19 -0600

    calvin typing, jonathan brain working fine i guess

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,20 +1,40 @@
 import java.util.ArrayList;
 import java.io.FileReader;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.IOException;
 
 class ScoreTrakker {
-    private ArrayList<Student> studentRecord = new ArrayList<Student>();
+    private ArrayList<Student> students = new ArrayList<Student>;
 
+    public ScoreTrakker() {
+        students = new ArrayList<>();
+    }
+    
     public void loadDataFile(string fileName) {
+        FileReader reader = new FileReader(fileName);
+        Scanner scanner = new Scanner(reader);
 
+        while (scanner.hasNextLine()) {
+            String name = scanner.nextLine();
+            int score = Integer.parseInt(scanner.nextLine());
+            students.add(new Student(name, score));
+        }
+       
+        scanner.close();
+        reader.close();
     }
 
     public void printInOrder() {
-
+        Collections.sort(students);
+        for (Student student : students) {
+            System.out.println(student);
+        }
     }
     
     public void processFiles() {
-        loadDataFile("scores.txt")
-        printInOrder()
+        loadDataFile("scores.txt");
+        printInOrder();
     }
 
     public static void main(String[] args) {
